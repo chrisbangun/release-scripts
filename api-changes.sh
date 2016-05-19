9c808773aa6ebe1a44fc9721a99d9ebb87bd75b3
Traveloka Engineering <adi.chris@traveloka.com>
2016-05-19 14:41:27 +0700

    fix bug in getting git full directory

--- api-changes.sh
+++ api-changes.sh
@@ -6,4 +6,4 @@ read -r current_branch
 printf "enter last week branch release: "
 read -r last_week_branch
 
-git --git-dir=/home/adi/tools/repository/.git --work-tree=/home/adi/tools/repository diff $current_branch $last_week_branch | grep +++ | grep api | grep -v impl | grep -v build.gradle | grep -v '/erp' | grep -v '/test/' | grep -v '/marketing/' | grep -v '/api/v1' | grep -v '/processing/' | grep -v '/hotel-extranet/' | grep -v 'Accessor.java'
+git --git-dir=/home/adi.chris/tools/repository/.git --work-tree=/home/adi.chris/tools/repository diff $current_branch $last_week_branch | grep +++ | grep api | grep -v impl | grep -v build.gradle | grep -v '/erp' | grep -v '/test/' | grep -v '/marketing/' | grep -v '/api/v1' | grep -v '/processing/' | grep -v '/hotel-extranet/' | grep -v 'Accessor.java'
