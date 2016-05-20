7b320d35d454b1f33b684d8f99a318cdad34d81b
Traveloka Engineering <adi.chris@traveloka.com>
2016-05-20 14:23:05 +0700

    script can now check all services version at production machine.

--- print_version.sh
+++ print_version.sh
@@ -2,7 +2,25 @@
 
 
 print_version_all(){
-  services=("tv" "tap" "frs" "fb" "fops" "pg" "hinv" "ne" "nei" "hops" "tripops")
+  
+  services=("tv" "fops" "hops" "tripops" "pg" "fb" "frs" "hinv" "ne" "nei" "tap")
+  servicesProduction=("tv10" "fops01" "hops-app-01" "tripops-app-02" "payment01x" "fb04" "frs14" "hinv01" "ne04" "nei04" "tap01")
+  
+  if [ "$2" == "production" ]; then
+    i=0
+    while [ $i -lt ${#services[*]} ]; do
+      command=`ssh ${servicesProduction[$i]} cat /var/traveloka/running/${services[$i]}/WEB-INF/classes/build.properties`
+      if [ $? -ne 0 ]; then
+        echo "could not execute $command"
+      else
+        echo "${services[$i]^^}:$command"
+        echo
+      fi
+      i=$(( $i+1));
+    done
+    exit
+  fi
+  
   for service in "${services[@]}"; do
     command=`ssh $2 cat /var/traveloka/running/$service/WEB-INF/classes/build.properties`
     if [ $? -ne 0 ]; then
@@ -32,9 +50,9 @@ print_version(){
   fi
 }
 
-if [ $# -ne 2 ];
+if [ $# -ne 2 ];	
 then
-  echo "usage: print_version (service/all) (stagingXX)"
+  echo "usage: print_version (service/all) (stagingXX/productionMachine)"
 else
   print_version $1 $2
 fi
